8b731e0a1be88af195917f4a79888d7e54c424b3
Veiasai <917971502@qq.com>
2017-12-14 20:54:51 +0800

    add home page

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,8 @@
  * Created by Veiasai on 2017/12/13.
  */
 import React from 'react';
-import {Navbar} from 'react-bootstrap'
+import {Navbar, Button, Grid, Col} from 'react-bootstrap'
+import {view as Sign} from './Sign'
 import {view as Chatbot} from './chatbot'
 import {view as Home} from './Home'
 import {
@@ -16,14 +17,23 @@ const App = () => (
     <Router>
         <div>
             <Navbar inverse >
-                <Navbar.Brand><a href="/">Programing</a></Navbar.Brand>
-                <Navbar.Brand><Link to="/home">Home</Link></Navbar.Brand>
-                <Navbar.Brand><Link to="/chatbot">Chatbot</Link></Navbar.Brand>
+                <Grid>
+                <Col md={8}>
+                    <Navbar.Brand><a href="/">Deep-learning</a></Navbar.Brand>
+                    <Navbar.Brand><Link to="/home">Docs</Link></Navbar.Brand>
+                    <Navbar.Brand><Link to="/chatbot">Chatbot</Link></Navbar.Brand>
+                </Col>
+                <Col md={4}>
+                    <Navbar.Brand><Button>Search</Button></Navbar.Brand>
+                    <Navbar.Brand />
+                    <Navbar.Brand><input/></Navbar.Brand>
+                </Col>
+                </Grid>
             </Navbar>
             <hr/>
-
-            <Route exact path="/home" component={Home}/>
+            <Route path="/home" component={Home}/>
             <Route path="/chatbot" component={Chatbot}/>
+            <Route exact path="/" component={Sign}/>
         </div>
     </Router>
 );
