71da7a1fab4c9058006caafdab1f2c098502d989
Veiasai <917971502@qq.com>
2017-12-15 20:12:24 +0800

    add docs

---
+++ web/src/Docs/views/Tensorflow.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Table} from "react-bootstrap"
+
+export default () => (
+    <Table striped condensed hover border="0" cellspacing="0" cellpadding="0" width="100%" height="100%">
+        <tbody>
+        <tr>
+            <td padding="10px" valign="top" align="left">
+                <h1>TensorFlow</h1>
+                <p>I don't know what to put here.</p>
+            </td>
+        </tr>
+        </tbody>
+    </Table>
+)
