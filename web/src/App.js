eef755fbc1d52753210d951eded8a1250ddb38fe
Veiasai <917971502@qq.com>
2017-12-15 13:29:22 +0800

    fix phone broswer visit

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,7 @@
  * Created by Veiasai on 2017/12/13.
  */
 import React from 'react';
-import {Navbar, Button, Grid, Col} from 'react-bootstrap'
+import {Navbar, Button, Grid, Col, InputGroup, FormControl, Clearfix} from 'react-bootstrap'
 import {view as Sign} from './Sign'
 import {view as Chatbot} from './chatbot'
 import {view as Home} from './Home'
@@ -18,15 +18,21 @@ const App = () => (
         <div>
             <Navbar inverse >
                 <Grid>
-                <Col md={8}>
-                    <Navbar.Brand><a href="/">Deep-learning</a></Navbar.Brand>
+                <Col md={8} xs={12}>
+                    <Navbar.Brand><Link to="/">Deep-learning</Link></Navbar.Brand>
                     <Navbar.Brand><Link to="/home">Docs</Link></Navbar.Brand>
                     <Navbar.Brand><Link to="/chatbot">Chatbot</Link></Navbar.Brand>
                 </Col>
-                <Col md={4}>
-                    <Navbar.Brand><Button>Search</Button></Navbar.Brand>
-                    <Navbar.Brand />
-                    <Navbar.Brand><input/></Navbar.Brand>
+
+                <Col md={4}xs={12}>
+                    <Navbar.Brand>
+                    <InputGroup>
+                        <InputGroup.Button>
+                            <Button>Search</Button>
+                        </InputGroup.Button>
+                        <FormControl type="text" />
+                    </InputGroup>
+                    </Navbar.Brand>
                 </Col>
                 </Grid>
             </Navbar>
