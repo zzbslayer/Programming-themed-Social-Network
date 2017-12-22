954c0d985f0ed942030f34482aa2a2f79a96f0a0
zzbslayer <zzbslayer@sjtu.edu.cn>
2017-12-22 23:21:36 +0800

    add context

--- web/src/Docs/views/Deeplearning.js
+++ web/src/Docs/views/Deeplearning.js
@@ -7,7 +7,9 @@ export default () => (
         <tr>
             <td padding="10px" valign="top" align="left">
                 <h1>Deep Learning</h1>
-                <p>I don't know what to put here.</p>
+                <p>Deep learning (also known as deep structured learning or hierarchical learning) is part of a broader family of machine learning methods based on learning data representations, as opposed to task-specific algorithms. Learning can be supervised, semi-supervised or unsupervised.</p>
+                <p>Some representations are loosely based on interpretation of information processing and communication patterns in a biological nervous system, such as neural coding that attempts to define a relationship between various stimuli and associated neuronal responses in the brain.</p>
+                <p>Deep learning architectures such as deep neural networks, deep belief networks and recurrent neural networks have been applied to fields including computer vision, speech recognition, natural language processing, audio recognition, social network filtering, machine translation, bioinformatics and drug design, where they have produced results comparable to and in some cases superior to human experts.</p>
             </td>
         </tr>
         </tbody>
